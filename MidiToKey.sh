2b4f3def593a03fbba9697a1154fe5c4be72b7b2
= <=>
2023-12-15 13:03:04 -0700

    add configuation script

--- MidiToKey.sh
+++ MidiToKey.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+test -d /usr/local/share/alsa
+
+if ($? == 1)
+{
+    sudo mkdir /usr/local/share/alsa/
+    sudo ln -s /usr/local/share/alsa/alsa.conf /usr/share/alsa/alsa.conf
+}
+
 /bin/python3 ./MidiToKey.py
