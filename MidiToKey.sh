c63e16d22e9adf3882f589fe813a10b63b345454
= <=>
2023-12-15 13:48:39 -0700

    fix setup script

--- MidiToKey.sh
+++ MidiToKey.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-test -f /usr/local/share/alsa/alsa.conf
+test -d /usr/local/share/alsa/
 
 if [ $? = 1 ]
 then
-    sudo mkdir /usr/local/share/alsa/
-    sudo ln -s /usr/share/alsa/alsa.conf  /usr/local/share/alsa/alsa.conf
+    sudo ln -s /usr/share/alsa/  /usr/local/share/alsa
+fi
+
+test -f /usr/local/lib/alsa-lib/libasound_module_conf_pulse.so
+
+if [ $? = 1 ]
+then
+    sudo mkdir /usr/local/lib/alsa-lib/
+    sudo ln -s /usr/lib/x86_64-linux-gnu/alsa-lib/libasound_module_conf_pulse.so /usr/local/lib/alsa-lib/libasound_module_conf_pulse.so
 fi
 
 /bin/python3 ./MidiToKey.py
